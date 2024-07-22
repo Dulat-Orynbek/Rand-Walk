0c0c0f6543c5d94e097d262868035682fdef848b
Dulat-Orynbek <dulat.orynbek777@gmail.com>
2024-07-22 16:47:40 +0500

    Update random_walk_check.R

--- random_walk_check.R
+++ random_walk_check.R
@@ -1,14 +1,4 @@
-install.packages('quantmod')
 
-require('quantmod')
-
-getSymbols("^GSPC", src = 'yahoo')
-getSymbols("AAPL", src = 'yahoo')
-head(GSPC)
-head(AAPL)
-
-
-acf(diff(Ad(GSPC)), na.action = na.omit)
 install.packages('quantmod')
 
 require('quantmod')
